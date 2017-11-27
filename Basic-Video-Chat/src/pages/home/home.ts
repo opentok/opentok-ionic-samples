f2d61731d0ce0e1414fbead91bac6f580eb27475
Manik <msach22@gmail.com>
2017-11-27 12:41:22 -0800

    add sample code for 1:1 basic-video-chat

--- Basic-Video-Chat/src/pages/home/home.ts
+++ Basic-Video-Chat/src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var OT:any;
+declare var Cordova: any;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  session: any;
+  publisher: any;
+  apiKey: any;
+  sessionId: string;
+  token: string;
 
   constructor(public navCtrl: NavController) {
+    this.apiKey = '';
+    this.sessionId = '';
+    this.token = '';
+  }
 
+  startCall() {
+    this.session = OT.initSession(this.apiKey, this.sessionId);
+
+    this.session.on({
+      streamCreated: (event) => {
+        this.session.subscribe(event.stream, 'subscriber');
+      }
+    });
+
+    this.session.connect(this.token, () => {
+      this.publisher = OT.initPublisher('publisher');
+      this.session.publish(this.publisher);
+    });
   }
 
 }
